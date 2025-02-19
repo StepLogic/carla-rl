c4b170a3c70ca3071f86957a910e91f6084fde31
Elvis Ohemeng Gyaase <48416104+StepLogic@users.noreply.github.com>
2025-02-19 13:11:17 -0500

    update:add test script

---
+++ src/leo/leo_env.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+from datetime import datetime
+import gymnasium as gym
+import rospy 
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image,Imu
+from geometry_msgs.msg import Twist,Vector3
+import queue
+from gymnasium.spaces import Dict,Box
+import numpy as np
+IMAGE_TOPIC="/camera/image_raw"
+IMU_TOPIC="/imu/data_raw"
+ROBOT_CMD_TOPIC="/cmd_vel"
+RATE = 9
+def ros_vector3_to_np_array(msg):
+    return np.array([msg.x,msg.y,msg.z])
+class LeoEnv(gym.Env):
+    def __init__(self):
+        self.image_sub = rospy.Subscriber(IMAGE_TOPIC,Image,self.image_callback)
+        self.imu_sub = rospy.Subscriber(IMU_TOPIC,Imu,self.imu_calback)
+        self.robot_cmd =rospy.Publisher(ROBOT_CMD_TOPIC, Twist, queue_size=10)
+        self.bridge = CvBridge()
+        self.image_queue=queue.Queue() #define queue
+        self.vector_queue=queue.Queue() #define quue for imu
+        # self.current_timestamp=datetime.now()
+        self.current_velocity = 0
+        self.current_heading = 0
+        self.target_speed=0.1
+        self.max_steer=1.0
+        self.image_size=64
+        self.theta=np.zeros((3,))
+        self.v=np.zeros((3,))
+        self.previous_actions=np.zeros((2)) #steer ,throttle
+        self.heading=0
+        self.idle_count=0
+        self.collision_threshold=300
+        self.time_episode =0
+        self.velocities=[]
+        self.dts=[]
+        self.headings=[]
+        self.distance_travelled=0
+        self.action=None
+        self.info=dict()
+        self.rewards=[]
+        image_space = Box(
+            low=-1.0,
+            high=1.0,
+            shape=(self.image_size, self.image_size, 3,),
+            dtype=np.float32,
+        )
+        
+        vec_space = Box(
+            low=-5.1,
+            high=5.1,
+            shape=(4,),
+            dtype=np.float32,
+        )
+
+        self.observation_space=Dict({"pixels":image_space, "vector":vec_space})
+
+        self.action_space = Box(
+            low=np.array([-self.max_steer, -1.0]),  # [steering, throttle/brake]
+            high=np.array([self.max_steer, 1.0]),
+            dtype=np.float32
+        )
+        self.rate = rospy.Rate(RATE)
+        self.RATE=RATE
+
+    def get_observation(self,reset=False):
+        # image=self.observation_space["pixels"].sample()
+
+        # vector=self.observation_space["vector"].sample()
+        image=None
+        vector=None
+
+        timeout=int(1e3)
+        for _ in range(int(timeout)):
+            # print(self.vector_queue.qsize())
+            vector=self.vector_queue.get()
+            if not vector is None:
+                self.vector_queue.queue.clear()
+                break
+            # if vector.header.timestamp==self.current_timestamp:
+            #     break
+        # timeout=0
+        for _ in range(int(timeout)):
+            image=self.image_queue.get()
+            if not image is None:
+                self.image_queue.queue.clear()
+                break
+            # if reset:
+            #     break
+
+        return dict(pixels=image,vector=vector)
+    
+    def reset(self,*args,**kwargs):
+        observation=self.get_observation(reset=True)
+        self.time_episode =0
+        self.distance_travelled=0
+        self.rewards=[]
+        self.action=np.zeros(2)
+        self.info=dict()
+        self.speed=0.0
+        return observation,self.info
+            
+    def image_callback(self,image):
+        # if image.header.timestamp >= self.current_timestamp: #look up
+        #         return
+        # try:
+        image = self.bridge.imgmsg_to_cv2(image, "rgb8")
+        image=cv2.resize(image,(self.image_size,self.image_size))
+        self.image_queue.put(image)
+        # except Exception as e:
+        #     print(e)
+            
+    def imu_calback(self,imu):
+        dt=1/self.RATE
+        # imu=imu.data
+        vec=self.observation_space["vector"].sample()
+        if dt > int(1e3): #look up
+                return
+        # try:
+
+        self.theta=self.theta+ros_vector3_to_np_array(imu.angular_velocity)*dt
+        self.v=self.v+ros_vector3_to_np_array(imu.linear_acceleration)*dt
+        # print(self.v,np.linalg.norm(self.v))
+        self.speed=np.linalg.norm(self.v)
+        vec[0]=self.previous_actions[0]/self.max_steer
+        vec[1]=self.previous_actions[1]/self.max_steer
+        vec[2]=np.clip(self.speed/self.target_speed+1e-8,-5.1,5.1)
+        vec[3]=np.clip(self.theta[-1]/self.heading+1e-8,-5.1,5.1)
+        self.vector_queue.put(vec)
+        self.velocities.append(self.v)
+        self.dts.append(dt)
+        self.distance_travelled+=max(np.mean(self.velocities)*np.mean(dt)*np.cos(self.theta[-1]),0)
+        # except Exception as e:
+        #     print(e)
+    def step(self,action:np.ndarray):
+        self.action=action
+        vel_msg = Twist()
+        vel_msg.linear.x = action[1]
+        vel_msg.angular.z = action[0]
+        print(vel_msg)
+        self.robot_cmd.publish(vel_msg)
+        observation=self.get_observation()
+        done=self.compute_done()
+        reward=self.compute_reward()
+        self.rate.sleep()
+        return observation, reward, done,False,self.info     
+    def compute_done(self):
+        self.info=dict()
+        self.collision = self.collision_threshold < self.idle_count
+        if np.linalg.norm(self.v) > 1e-4:
+            self.idle_count = 0
+        else:
+            self.idle_count += 1
+        self.time_episode += 1
+               
+        # hero_velocity = self.get_speed(hero)
+        # marker_location=sensor_data["goal_heading"][-1][0]
+        # wp=core.map.get_waypoint(hero.get_transform().location,project_to_road=False) 
+        self.done_dist = self.distance_travelled>200
+        self.done_speed=(self.speed/(self.target_speed+1e-8)) > 5.0
+        # self.done_falling = hero.get_location().z < -0.5
+        # self.diff_lane = 'lane_invasion' in sensor_data.keys()
+        # self.collision = 'collision' in sensor_data.keys()
+        # self.done_speed=(hero_velocity/(self.target_speed+1e-8)) > 5.0
+        done= self.done_dist or self.collision or self.done_speed
+        self.info.update(dict(is_success=self.done_dist,
+                             distance_completed=self.distance_travelled,
+                             max_reward=0,
+                             min_reward=0,
+                             mean_reward=0
+                             ))
+        if len(self.rewards)>0:
+                    self.info.update(dict(
+                             max_reward=np.max(self.rewards),
+                             min_reward=np.min(self.rewards),
+                             mean_reward=np.mean(self.rewards)))
+        # if done:
+        #     self.info.update(is_success=self.done_dist,
+        #                      distance_completed=self.distance_travelled,
+        #                      max_reward=np.max(self.rewards),
+        #                      min_reward=np.min(self.rewards),
+        #                      mean_reward=np.mean(self.rewards))
+            # print(self.distance_travelled)
+        return done
+    def compute_reward(self):
+        # hero = core.hero
+        heading=self.heading
+        imu=self.theta[-1]
+
+
+        hero_velocity = self.speed
+
+
+        speed_factor=np.exp(-(hero_velocity-self.target_speed)**2)
+
+        # Normalize heading error to [-1.0, 1.0] to allow for larger corrections
+        heading_factor = np.exp(-(imu-heading)**2)
+        # heading_error = np.clip(heading_error, -1.0, 1.0)
+
+        # Calculate smooth action penalty to encourage smoother control inputs
+        action_factor = np.exp(-np.sum((self.previous_actions-self.action)**2))
+
+
+        reward=  2.0*speed_factor + 0.5*action_factor + heading_factor
+        # print(speed_factor,self.target_speed)
+        # reward=-1e-3
+        # if hero_velocity<self.target_speed:
+        #     reward += delta_distance
+        # else:
+        #     reward+=0
+        # reward=  target_speed_error*0.5 + heading_error + smooth_action*0.1
+        # print(reward,target_speed_error,heading_error)
+        # Penalize falling, collisions, lane invasions, and excessive speed
+        if self.collision:
+            print(f'Collision Dist={self.distance_travelled:3f} Target_S={self.target_speed:.4f} Vel={hero_velocity:.4f} R={reward:.4f} ')
+            reward += -10
+        if self.done_speed:
+            # print(f'Too fast Smooth={smooth_action:3f} Dist={self.distance_travelled:3f} Ratio={hero_velocity/self.target_speed:.3f} Target_S={self.target_speed:.3f} Vel={hero_velocity:.3f} R={reward:.4f} Err={target_speed_error:.4f} H_Err={heading_error:.4f}')
+            reward += -10
+        # Reward for reaching the target distance
+        if self.done_dist:
+            # print(f"Max Dist Smooth={smooth_action:3f} Dist={self.distance_travelled:3f}")
+            reward += 10
+
+        # Scale the reward to a reasonable range (no need for *10)
+        # reward = np.clip(reward, -2.0, 2.0)
+        # reward*=10
+# 
+        # Store the reward for logging or analysis
+        self.rewards.append(reward)
+
+        # Update previous actions for smoothness calculation
+        self.previous_actions=self.action
+        return reward
+        
---
+++ test_env.py
@@ -0,0 +1,14 @@
+import rospy
+from src.leo.leo_env import LeoEnv
+import numpy as np
+import cv2
+rospy.init_node("PD_CONTROLLER", anonymous=False)
+env=LeoEnv()
+observation,info=env.reset()
+res=cv2.imwrite("test.jpg",observation["pixels"])
+# print(observation["vector"],res)
+while not rospy.is_shutdown():
+    next_observation, reward, done, truncated, info=env.step(np.array([0.2,0.1]))
+    # cv2.imwrite("test.jpg",next_observation["pixels"])
+
+    # print(next_observation["vector"])
